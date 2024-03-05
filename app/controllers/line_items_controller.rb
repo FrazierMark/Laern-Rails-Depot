3476b756bb6f1d1399a6cf79aca987e65f1999ef
FrazierMark <m.mark.frazier@gmail.com>
2024-03-05 11:37:52 -0800

    Use quantity for line items

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -23,17 +23,20 @@ class LineItemsController < ApplicationController
 
   # POST /line_items or /line_items.json
   def create
-    # find product using the params from the request
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to line_item_url(@line_item), notice: "Line item was successfully created." }
-        format.json { render :show, status: :created, location: @line_item }
+        format.html { redirect_to cart_url(@line_item.cart),
+          notice: "Line item was successfully created." }
+        format.json { render :show,
+          status: :created, location: @line_item }
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.html { render :new,
+          status: :unprocessable_entity }
+        format.json { render json: @line_item.errors,
+          status: :unprocessable_entity }
       end
     end
   end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,4 +3,14 @@ class Cart < ApplicationRecord
   # items is dependent on the existence of the cart
   # If we destroy the cart, we want Rails to destroy any line items
   has_many :line_items, dependent: :destroy
+
+  def add_product(product)
+    current_item = line_items.find_by(product_id: product.id)
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(product_id: product.id)
+    end
+    current_item
+  end
 end
