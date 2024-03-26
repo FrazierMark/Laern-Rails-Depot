1937a4b20390f16cd0099b42e75d7b48d69348d2
FrazierMark <m.mark.frazier@gmail.com>
2024-03-26 08:31:24 -0700

    Create a stimulus controller that shows additional payment fields based on selection.

--- app/javascript/controllers/payment_controller.js
+++ app/javascript/controllers/payment_controller.js
@@ -2,6 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="payment"
 export default class extends Controller {
-  connect() {
+
+  // Declare a list of targets that identify HTML elements that our controller will interact with
+  static targets = [ "selection", "additionalFields" ]
+
+  initialize() {
+    this.showAdditionalFields()
+  }
+
+  showAdditionalFields() {
+    let selection = this.selectionTarget.value
+
+    for (let fields of this.additionalFieldsTargets) {
+      fields.disabled = fields.hidden = (fields.dataset.type != selection)
+    }
   }
 }
