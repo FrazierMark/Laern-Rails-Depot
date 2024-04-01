51121226f585eed1b061754cc61c897e15cc7277
FrazierMark <m.mark.frazier@gmail.com>
2024-04-01 10:38:46 -0700

    migration to add price to line_items, system tests

--- app/javascript/controllers/payment_controller.js
+++ app/javascript/controllers/payment_controller.js
@@ -4,6 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   // Declare a list of targets that identify HTML elements that our controller will interact with
+  // Targets are a selection element and additional fields
   static targets = [ "selection", "additionalFields" ]
 
   initialize() {
